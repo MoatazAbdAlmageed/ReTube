23a7ed6b8e5214f2c6194b9bbf4ca4da234dc07c
MoatazGitHub <moataz.mohammady@gmail.com>
2018-06-20 22:47:16 +0200

    Use images instead of ifram in videoList

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -5,21 +5,18 @@ export class VideoListItem extends Component {
     constructor(props) {
         super(props);
         // console.log(this.props.video);
-        this.src = "https://www.youtube.com/embed/"+this.props.video.id.videoId;
-        console.log(this.src);
+        /*this.src = "https://www.youtube.com/embed/"+this.props.video.id.videoId;
+        console.log(this.src);*/
+        this.src = this.props.video.snippet.thumbnails.medium.url;
+        this.title = this.props.video.snippet.title;
     }
 
     render() {
         return (
 
             <div className="VideoListItem">
-                <div className="card">
-                    <iframe width="100%" src={this.src}
-                            frameBorder="0"
-                            showinfo="0" controls="0"
-                    ></iframe>
-
-                </div>
+                    <a href="#"><img src={this.src} alt={this.title}/>
+                    </a>
             </div>
         );
     }
